f692969cec4dd925a94e35df64b9f9cb13d380fa
pongpatapee <pongpatapee1021@gmail.com>
2022-01-22 10:25:06 -0500

    got firestore working

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, json, jsonify, send_from_directory, send_file
 from flask.helpers import make_response
 from flask_cors import CORS  # comment this on deployment
-from google.cloud import firestore
 import os
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("service_key.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app)  # comment this on deployment
 
-credential_path = 'service_key.json'
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
-
-db = firestore.Client(project='BoilerMake-WhatsInMyFridge')
 
 @app.route('/', methods=['GET'])
 def home():
@@ -18,11 +21,15 @@ def home():
 
 @app.route('/test')
 def test():
-    doc_ref = db.collection(u'users').document(u'alovelace')
+    doc_ref = db.collection(u'users').document(u'aturing')
     doc_ref.set({
-        u'first': u'Ada',
-        u'last': u'Lovelace',
-        u'born': 1815
-    })   
+        u'first': u'Alan',
+        u'middle': u'Mathison',
+        u'last': u'Turing',
+        u'born': 1912
+    })
+
+    return 'success'
+
 if __name__ == '__main__':
     app.run(debug=True)
